f2f1ecbccd530501ffaff747c30738099c2d1a49
Renan <renan23eduardo@gmail.com>
2023-01-08 11:11:08 -0300

    Stall dependencies for authentication

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -11,31 +11,48 @@ import {
 import { InputMovie } from './movie/movie.interface';
 import { MovieService } from './movie.service';
 import { Movie } from './movie.entity/movie.entity';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger/dist/decorators';
 
+@ApiTags('movies')
 @Controller('movie')
 export class MovieController {
   constructor(private readonly movieService: MovieService) {}
 
+  @ApiOkResponse({
+    description: 'All available movies',
+  })
   @Get()
   getAllMovies(): Promise<Movie[]> {
     return this.movieService.getAllMovies();
   }
 
+  @ApiOkResponse({
+    description: 'The movie matching the given id',
+  })
   @Get(':id')
   getOneMovie(@Param('id') id: string): Promise<Movie> {
     return this.movieService.getOneMovie(parseInt(id, 10));
   }
 
+  @ApiOkResponse({
+    description: 'The newly created movie',
+  })
   @Post()
   createNewMovie(@Body() movie: InputMovie) {
     return this.movieService.createNewMovie(movie);
   }
 
+  @ApiOkResponse({
+    description: 'The updated movie',
+  })
   @Put(':id')
   updateMovie(@Param('id') id: string, @Body() movie: Movie): Promise<Movie> {
     return this.movieService.updateMovie(parseInt(id, 10), movie);
   }
 
+  @ApiOkResponse({
+    description: 'Nothing',
+  })
   @Delete(':id')
   @HttpCode(204)
   removeMovie(@Param('id') id: string): void {
--- src/movie/movie.entity/movie.entity.ts
+++ src/movie/movie.entity/movie.entity.ts
@@ -1,13 +1,26 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger/dist';
 
 @Entity('Movie')
 export class Movie {
+  @ApiProperty({
+    description: 'Id of the movie',
+    example: 1,
+  })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({
+    description: 'Title of the movie',
+    example: 'Iron Man',
+  })
   @Column()
   title: string;
 
+  @ApiProperty({
+    description: 'Year the movie was released',
+    example: 2008,
+  })
   @Column()
   year: number;
 }
